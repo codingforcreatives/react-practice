85dd76aeef49ae2b5cbf4198fc931ac3a38ed2b3
Clarentia Mursalim <clary@Clarentias-MacBook-Pro.local>
2020-09-03 18:28:35 +1000

    Updates

---
+++ precache-manifest.b9dc40de5b49db4e419295a31da415eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfdb10a3ee619280b70c35293b79d49",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "0ccea26e6a3a99520cea",
+    "url": "/react-practice/static/css/main.95d04006.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/react-practice/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccea26e6a3a99520cea",
+    "url": "/react-practice/static/js/main.c9217e92.chunk.js"
+  },
+  {
+    "revision": "584ac8c0b069300d3df7",
+    "url": "/react-practice/static/js/runtime-main.72aa8097.js"
+  }
+]);
